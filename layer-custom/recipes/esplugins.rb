28c4949a99857fe961814eef2f337c2c449cfeca
Makapen Co <makapen@users.noreply.github.com>
2015-06-21 00:01:42 -0700

    Added Kibana and Kopf plugins

--- layer-custom/recipes/esplugins.rb
+++ layer-custom/recipes/esplugins.rb
@@ -4,6 +4,9 @@ script "install_plugin_es_head" do
 	cwd "#{node.elasticsearch[:dir]}/elasticsearch-#{node.elasticsearch[:version]}/bin/"
 	code <<-EOH
   	plugin -install mobz/elasticsearch-head
+  	plugin -install kibana-community/kibana3
+  	plugin -install kibana-community/kibana4-static
+  	plugin -install lmenezes/elasticsearch-kopf
   	EOH
 	not_if { File.exist?("#{node.elasticsearch[:dir]}/elasticsearch-#{node.elasticsearch[:version]}/plugins/head") }
 end
