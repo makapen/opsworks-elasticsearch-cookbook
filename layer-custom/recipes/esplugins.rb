2f95ce5a54baef7427c7d14b6d7c2f8f89d03230
Makapen Co <makapen@users.noreply.github.com>
2015-06-21 16:41:22 -0700

    Set kibana4 plugin to point to makapen branch

--- layer-custom/recipes/esplugins.rb
+++ layer-custom/recipes/esplugins.rb
@@ -5,7 +5,7 @@ script "install_plugin_es_head" do
 	code <<-EOH
   	plugin -install mobz/elasticsearch-head
   	plugin -install kibana-community/kibana3
-  	plugin -install kibana-community/kibana4-static
+  	plugin -install makapen/kibana4-static
   	plugin -install lmenezes/elasticsearch-kopf
   	EOH
 	not_if { File.exist?("#{node.elasticsearch[:dir]}/elasticsearch-#{node.elasticsearch[:version]}/plugins/head") }
